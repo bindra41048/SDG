2e9130117195c17788986af08ad19ab25f2acb5a
Alex Wade <dendanacode@gmail.com>
2018-04-12 17:49:00 -0700

    filler text is now SDG-related

--- sdg-site/routes/index.js
+++ sdg-site/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'Express',
+    title: 'SDG Site',
     lat: 37.3382,
     lng: -121.8863,
     key: 'pk.eyJ1Ijoic3RhbmZvcmRzdXMiLCJhIjoiY2pmcjhtenJ5MGh4ZzMycDd0ajkxMHZobiJ9.JU52RKwVG17CJx1Cyj9Siw'
