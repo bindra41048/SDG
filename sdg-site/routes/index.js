19dd46025ed20f5c4b3f619c5787f2191f1037b2
Alex Wade <dendanacode@gmail.com>
2018-04-27 12:50:25 -0700

    lmao error page now actually shows error, what a concept

--- sdg-site/routes/index.js
+++ sdg-site/routes/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+
+const env = {
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  AUTH0_CALLBACK_URL:
+    process.env.AUTH0_CALLBACK_URL
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,5 +18,30 @@ router.get('/', function(req, res, next) {
     key: 'pk.eyJ1Ijoic3RhbmZvcmRzdXMiLCJhIjoiY2pmcjhtenJ5MGh4ZzMycDd0ajkxMHZobiJ9.JU52RKwVG17CJx1Cyj9Siw'
   });
 });
+
+router.get('/login', passport.authenticate('auth0', {
+  clientID: env.AUTH0_CLIENT_ID,
+  domain: env.AUTH0_DOMAIN,
+  redirectUri: env.AUTH0_CALLBACK_URL,
+  responseType: 'code',
+  audience: 'https://' + env.AUTH0_DOMAIN + '/userinfo',
+  scope: 'openid profile'}),
+  function(req, res) {
+    res.redirect("/");
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/callback',
+  passport.authenticate('auth0', {
+    failureRedirect: '/failure'
+  }),
+  function(req, res) {
+    res.redirect(req.session.returnTo || '/user');
+  }
+);
 
 module.exports = router;
