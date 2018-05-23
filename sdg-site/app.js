98d86fc5fe502fd55f182ac62dcec743c3962737
Alex Wade <dendanacode@gmail.com>
2018-05-23 14:20:38 -0700

    one more cleanup

--- sdg-site/app.js
+++ sdg-site/app.js
@@ -60,7 +60,6 @@ app.set('view engine', "hbs");
 var hbs = require('handlebars');
 hbs.registerHelper("compactFn", function(context, options) {
       var ret = "<ul>";
-      console.log(context.length);
       for (var i = 0, j = context.length; i < j; i++) {
         ret = ret + "<li>" + options.fn(context[i]) + "</li>";
       }
