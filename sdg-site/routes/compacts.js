ed308764376d487aad510e33616c51782e0e193f
Alex Wade <dendanacode@gmail.com>
2018-05-26 16:13:34 -0700

    can now view one specific compact and edit it!

--- sdg-site/routes/compacts.js
+++ sdg-site/routes/compacts.js
@@ -14,7 +14,8 @@ router.get('/view',
         view: "Grid view"
     }).eachPage(function page(records, fetchNextPage) {
         records.forEach(function(record) {
-            var id = record.get('id');
+            console.log(record.id);
+            var id = record.id;
             var year = record.get('year');
             var name = record.get('name');
             compacts_struct.push({'id': id, 'year': year, 'name': name});
@@ -49,10 +50,48 @@ router.post('/new',
       "year": req.body.year_field,
       "name": req.body.name_field
     }, function (err, record) {
-      if (err) { console.error(err); return; }
-      console.log(record.getId());
+      if (err) { console.error(err); return; } //need to make more robust
+      //console.log(record.getId());
     });
     res.redirect('view');
+    //TODO: validate lol
+});
+
+router.get('/:id',
+  function(req, res, next) {
+    var id = req.params.id;
+    base('Compacts').find(id, function(err, record) {
+      if (err) {console.error(err); return;} //need to make more robust
+      res.render('one_compact', {
+        compact: {'id': id, 'year': record.get('year'), 'name': record.get('name')},
+        user: req.session.user,
+        edit_url: "/compacts/edit/" + id
+      });
+    });
+});
+
+router.get('/edit/:id',
+  function(req, res, next) {
+    var id = req.params.id;
+    base('Compacts').find(id, function(err, record) {
+      if (err) {console.error(err); return;} //need to make more robust
+      res.render('edit_compact', {
+        compact: {'id': id, 'year': record.get('year'), 'name': record.get('name')},
+        user: req.session.user
+      });
+    });
+});
+
+router.post('/edit/:id',
+  function(req, res, next) {
+    var id = req.params.id;
+    base('Compacts').update(id, {
+      "year": req.body.year_field,
+      "name": req.body.name_field
+    }, function (err, record) {
+      if (err) { console.error(err); return; } //need to make more robust
+    });
+    res.redirect('/compacts/' + id);
     //TODO: validate lol
 });
 
