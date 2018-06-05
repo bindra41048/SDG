6cd27f6a63e5863021ee5315d4392c3fee1d46c9
Ada Zhou <zhouc@stanford.edu>
2018-06-05 02:06:11 -0700

    Please enter the commit message for your changes. Lines starting

--- sdg-site/public/javascripts/display_milestones.js
+++ sdg-site/public/javascripts/display_milestones.js
@@ -2,26 +2,63 @@ function displayMilestones(milestones) {
 
   var ctx = document.getElementById("myChart").getContext('2d');
   ctx.scale(0.5, 0.5);
+
+  var current_year = 2018;
+
   var years = [];
-  var targets = [];
+  var historical_targets = [];
+  var future_targets = [];
+
+
+
   for (var i = 0; i < milestones.length; i++) {
     years.push(Number(milestones[i].year));
-    targets.push(Number(milestones[i].value));
+    if (milestones[i].year > current_year) {
+      historical_targets.push(null);
+    } else {
+      historical_targets.push(Number(milestones[i].value)); 
+    }
+     future_targets.push(Number(milestones[i].value));
+
   }
+
+
+  
+
+
   var myChart = new Chart(ctx, {
       type: 'line',
       data: {
           labels: years,
-          datasets: [{
-              label: 'Compact Targets',
-              data: targets,
+          datasets: [
+          {
+              label: 'Targets',
+              data: future_targets,
+              fill: false,
+              lineTension: 0,
+              borderDash: [10,5],
+              borderColor: "green",
+          },
+          {
+              label: 'Historical Targets',
+              data: historical_targets,
               fill: false,
               borderColor: "green",
+
+
           }]
       },
       options: {
-          scales: {
+          scales: {    
+
+                
+              
+
+
+            
+           
           }
+          
       }
   });
 
